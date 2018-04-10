c68c2a3f14b877ae05c277d50936e1759c489774
charleshamesse <charles.hamesse@gmail.com>
2018-04-10 13:15:48 +0200

    Added flickr photo fetching script

--- dataset/main.py
+++ dataset/main.py
@@ -1,21 +1,59 @@
+import json
 import requests
+import urllib
+import os
 
-
-'''
-Example request
-https://api.flickr.com/services/rest/?api_key=78d7c05a38eb68608887093fd60516b6&method=flickr.photos.search&sort=relevance&text=snake&per_page=500
-'''
+# API info
 API_KEY = '78d7c05a38eb68608887093fd60516b6'
-BASE_URL = 'https://api.flickr.com/services/rest/'
+API_URL = 'https://api.flickr.com/services/rest/'
+
+# Might need to create this folder
+DATA_FOLDER = './photos/'
 
 
-# Make a get request to get the latest position of the international space station from the opennotify api.
-response = requests.get(
-    BASE_URL + "?api_key=" + API_KEY \
-    + "&method=flickr.photos.search" \
-    + "&sort=relevance" \
-    + "&text=snake"
-)
+# Reconstruct photo url
+def get_photo_url(farm_id, server_id, photo_id, secret):
+    return 'https://farm' + str(farm_id) + '.staticflickr.com/' + str(server_id) + '/' + str(photo_id) + '_' + str(secret) + '_q.jpg'
 
-# Print the status code of the response.
-print(response.status_code)
+# Save image from url to disk
+def save_image(url, fname):
+    r = requests.get(url, stream=True)
+    with open(fname, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            f.write(chunk)
+        return True
+    return False
+
+#  Download all photos given a query text 
+def download_photos(query):
+    parameters = {
+        "api_key": API_KEY,
+        "format": "json",
+        "sort": "relevance", 
+        "method": "flickr.photos.search",
+        "text": query
+    }
+    response = requests.get(API_URL, parameters)
+
+    # Print the status code of the response.
+    if response.status_code == 200:
+        # Response is wrapped in "jsonFlickrApi()" so we remove it before parsing
+        json_data = json.loads(response.text[14:-1])
+
+        # For all photos
+        for photo in json_data['photos']['photo']:
+            # Build url and filename
+            url = get_photo_url(photo["farm"], photo["server"], photo["id"], photo["secret"])
+            filename = query + "_" + str(photo["id"]) + '.jpg'
+            # Try to save it
+            try:
+                save_image(url, os.path.join(DATA_FOLDER, filename))
+            except Exception as e:
+                print("Error: could not download " + filename, e)
+
+    else:
+        print("API call error")
+
+# Entry point
+if __name__ == '__main__':
+    download_photos("snake")
