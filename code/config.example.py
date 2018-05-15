7d9b332050597ce81c07847d188272ebf80025ab
charleshamesse <charles.hamesse@gmail.com>
2018-05-15 11:44:24 +0200

    Updated config

--- code/config.example.py
+++ code/config.example.py
@@ -1,8 +1,11 @@
-DATASET = {
+DATA = {
     'dataset_name': 'photos_108',
-    'checkpoint_dir': 'checkpoints',
-    'model_dir': 'models',
     'data_dir': '../dataset',
     'sample_dir': '../dataset/generated/',
-    'results_dir': 'results'
+    'results_dir': 'results',
+}
+
+MODEL = {
+    'model_dir': 'models',
+    'checkpoint_dir': 'checkpoints',
 }
